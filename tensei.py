93be908de94a14e1211ae25d102ac6c4d91ec9ca
S-Agil <iscysabsolute@yahoo.co.jp>
2017-12-06 17:18:37 +0900

    fix use file

--- tensei.py
+++ tensei.py
@@ -7,7 +7,7 @@ tagger = MeCab.Tagger('-Owakati')
 words = tagger.parse(obj)
 print('\n', words)
 
-classifier = ft.load_model('tensei3.bin')
+classifier = ft.load_model('tensei.bin')
 estimate_2 = classifier.predict_proba([words], k=2)
 if estimate_2[0][0][0] == "__label__1,":
     print('流行る！')
